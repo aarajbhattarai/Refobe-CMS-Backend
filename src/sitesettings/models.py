011d6738527d4f3da66b806712b4c9abecd18c77
Aaraj <aarajbhattarai@gmail.com>
2025-01-24 15:00:43 +0000

    Social Media Config added in Site Settings

--- src/sitesettings/models.py
+++ src/sitesettings/models.py
@@ -9,12 +9,25 @@ from wagtail.fields import RichTextField
 class SiteSetting(BaseSiteSetting):
     gtm_id = models.CharField(max_length=50, blank=True)
     google_site_verification = models.CharField(max_length=255, blank=True)
+    facebook = models.URLField(blank=True, null=True)
+    facebook_pixel = models.CharField(max_length=255, blank=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    linkedin_pixel= models.CharField(max_length=255, blank=True)
+    x_twitter = models.URLField(blank=True, null=True)
 
     cookie_content = RichTextField(
         blank=True, null=True, verbose_name=_("Cookie bar content"), features=[]
     )
 
-    panels = [FieldPanel("gtm_id"), FieldPanel("cookie_content")]
+    panels = [FieldPanel("gtm_id"), FieldPanel("cookie_content"),FieldPanel("facebook"),
+              FieldPanel("facebook_pixel"),
+              FieldPanel("instagram"),
+              FieldPanel("linkedin"),
+              FieldPanel("linkedin_pixel"),
+              FieldPanel("x_twitter"),
+              FieldPanel("google_site_verification"),
+              ]
 
     def __str__(self):
         return str(self.site)
